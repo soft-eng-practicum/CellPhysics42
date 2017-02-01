39bf1898d9a8e7d68638dce55b6cadd0a6ee305e
Bess Burnett <bburnett2@ggc.edu>
2017-02-01 11:58:50 -0500

    added view package

---
+++ src/cellPhysics42/MainApp.java
@@ -0,0 +1,16 @@
+package cellPhysics42;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/cellPhysics42/view/DemoVeiwControl.java
@@ -0,0 +1,16 @@
+package cellPhysics42.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class DemoVeiwControl {
+	@FXML
+	Label ruleName;
+	@FXML
+	GridPane displayGrid;
+	
+	public void setGridSize(int rows, int columns){
+		//displayGrid.setCo
+	}
+}
