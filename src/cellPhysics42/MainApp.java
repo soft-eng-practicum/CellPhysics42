b7895bb7ed0cc01ca8fe2416b9625d8d24bbc170
Bess Burnett <bburnett2@ggc.edu>
2017-02-02 18:32:10 -0500

    more work on GUI and added control class

--- src/cellPhysics42/MainApp.java
+++ src/cellPhysics42/MainApp.java
@@ -1,13 +1,27 @@
 package cellPhysics42;
 
+
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		
+		try{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(MainApp.class.getResource("view/DemoView.fxml"));
+		Scene scene = new Scene(loader.load());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		}
+		catch(IOException ex){
+			
+		}
 	}
 
 	public static void main(String[] args) {
--- src/cellPhysics42/view/DemoVeiwControl.java
+++ src/cellPhysics42/view/DemoVeiwControl.java
@@ -3,14 +3,40 @@ package cellPhysics42.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class DemoVeiwControl {
 	@FXML
-	Label ruleName;
+	private Label ruleName;
 	@FXML
-	GridPane displayGrid;
+	private GridPane displayGrid;
+	private int nextRow;
+	private int numRows;
+	private int numCols;
 	
-	public void setGridSize(int rows, int columns){
-		//displayGrid.setCo
+	public String getStringRow(byte[][] nextLineArray){
+		StringBuilder nextLine = new StringBuilder();
+		for(int i = 0; i < nextLineArray[1].length; i++){
+			nextLine.append(nextLineArray[1][i]);
+		}
+		return nextLine.toString();
+	}
+	
+	public void fillGrid(){
+		for(int i = 0; i < numRows; i++){
+			fillNextLine(getStringRow(getNext));
+			nextRow++;
+		}
+	}
+	
+	
+	public void fillNextLine(String nextLine){
+		for(int i = 0; i < nextLine.length(); i++){
+			if(nextLine.charAt(i) == '1'){
+				displayGrid.add(new Rectangle(displayGrid.getWidth()/numCols, displayGrid.getHeight()/numRows,
+						Color.BLACK), i, nextRow);
+			}
+		}
 	}
 }
