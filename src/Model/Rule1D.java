092f802fc45e918c967372f1d8b621528ad204af
Michael <mholtmann@ggc.edu>
2017-02-03 11:48:24 -0500

    Changes to the 1Drule class

--- src/Model/Rule1D.java
+++ src/Model/Rule1D.java
@@ -1,8 +1,8 @@
 package Model;
+
 import exception.NotValidRuleException;
 
-public class rule1D
-{
+public class Rule1D {
 	private byte state7;
 	private byte state6;
 	private byte state5;
@@ -12,52 +12,58 @@ public class rule1D
 	private byte state1;
 	private byte state0;
 	private int rule;
-	
-	public rule1D(int rule)
-	{
+	final private int[] rules = { 126, 10, 22, 50, 54, 90, 94, 122, 146, 150, 170, 182, 210, 222, 250, 254 };
+
+	public Rule1D(int rule) throws NotValidRuleException {
 		setRule(rule);
-		String binary = toBinary(rule);
-		state7 = Byte.parseByte(binary.substring(binary.length()-8, binary.length()-7));
-		state6 = Byte.parseByte(binary.substring(binary.length()-7, binary.length()-6));
-		state5 = Byte.parseByte(binary.substring(binary.length()-6, binary.length()-5));
-		state4 = Byte.parseByte(binary.substring(binary.length()-5, binary.length()-4));
-		state3 = Byte.parseByte(binary.substring(binary.length()-4, binary.length()-3));
-		state2 = Byte.parseByte(binary.substring(binary.length()-3, binary.length()-2));
-		state1 = Byte.parseByte(binary.substring(binary.length()-2, binary.length()-1));
-		state0 = Byte.parseByte(binary.substring(binary.length()-1, binary.length()));		
-	}
-	
 
-	public void setRule(int rule)
-	{
-		this.rule = rule;
 	}
 
+	public void setRule(int rule) throws NotValidRuleException {
+		boolean check = false;
+		for (int i : rules) {
+			if (i == rule) {
+				check = true;
+			}
+		}
 
-	public int getRule()
-	{
+		if (check) {
+			this.rule = rule;
+			String binary = toBinary(rule);
+			state7 = Byte.parseByte(binary.substring(binary.length() - 8, binary.length() - 7));
+			state6 = Byte.parseByte(binary.substring(binary.length() - 7, binary.length() - 6));
+			state5 = Byte.parseByte(binary.substring(binary.length() - 6, binary.length() - 5));
+			state4 = Byte.parseByte(binary.substring(binary.length() - 5, binary.length() - 4));
+			state3 = Byte.parseByte(binary.substring(binary.length() - 4, binary.length() - 3));
+			state2 = Byte.parseByte(binary.substring(binary.length() - 3, binary.length() - 2));
+			state1 = Byte.parseByte(binary.substring(binary.length() - 2, binary.length() - 1));
+			state0 = Byte.parseByte(binary.substring(binary.length() - 1, binary.length()));
+		} else {
+			throw new NotValidRuleException(rule + " is not a valid rule");
+		}
+	}
+
+	public int getRule() {
 		return rule;
 	}
 
+	public byte isOn(byte one, byte two, byte three) throws NotValidRuleException {
 
-	public byte isOn(byte one, byte two, byte three) throws NotValidRuleException
-	{
-		
-		if(one == 1 && two == 1 && three == 1)
+		if (one == 1 && two == 1 && three == 1)
 			return state7;
-		else if(one == 1 && two == 1 && three == 0)
+		else if (one == 1 && two == 1 && three == 0)
 			return state6;
-		else if(one == 1 && two == 0 && three == 1)
+		else if (one == 1 && two == 0 && three == 1)
 			return state5;
-		else if(one == 1 && two == 0 && three == 0)
+		else if (one == 1 && two == 0 && three == 0)
 			return state4;
-		else if(one == 0 && two == 1 && three == 1)
+		else if (one == 0 && two == 1 && three == 1)
 			return state3;
-		else if(one == 0 && two == 1 && three == 0)
+		else if (one == 0 && two == 1 && three == 0)
 			return state2;
-		else if(one == 0 && two == 0 && three == 1)
+		else if (one == 0 && two == 0 && three == 1)
 			return state1;
-		else if(one == 0 && two == 0 && three == 0)
+		else if (one == 0 && two == 0 && three == 0)
 			return state0;
 		else
 			throw new NotValidRuleException("Bad input for isOn method");
@@ -65,17 +71,17 @@ public class rule1D
 	}
 
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return "rule1D [state7=" + state7 + ", state6=" + state6 + ", state5=" + state5 + ", state4=" + state4
 				+ ", state3=" + state3 + ", state2=" + state2 + ", state1=" + state1 + ", state0=" + state0 + "]";
 	}
 
-
-
-	private String toBinary(int rule)
-	{
+	private String toBinary(int rule) {
 		return String.format("%16s", Integer.toBinaryString(rule)).replace(' ', '0');
+	}
+
+	public int[] getRules() {
+		return rules;
 	}
 
 }
--- src/Model/ruleModle.java
+++ src/Model/ruleModle.java
@@ -6,12 +6,12 @@ public class ruleModle
 
 	public static void main(String[] args)
 	{
-		rule1D rule = new rule1D(90);
-		System.out.println(rule.toString());
-		
-		
+
+
 		try
 		{
+			Rule1D rule = new Rule1D(90);
+			System.out.println(rule.toString());
 			System.out.println(rule.isOn((byte)1,(byte)1,(byte)1));
 			System.out.println(rule.isOn((byte)1,(byte)1,(byte)0));
 			System.out.println(rule.isOn((byte)1,(byte)0,(byte)1));
