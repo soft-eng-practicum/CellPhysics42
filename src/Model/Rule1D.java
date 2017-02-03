c09428af8f3e118df4fb9de9fd50c5cca47f2eaf
Michael <mholtmann@ggc.edu>
2017-02-03 14:31:21 -0500

    logic for the array in the 1DRule class

--- src/Model/Rule1D.java
+++ src/Model/Rule1D.java
@@ -13,10 +13,27 @@ public class Rule1D {
 	private byte state0;
 	private int rule;
 	final private int[] rules = { 126, 10, 22, 50, 54, 90, 94, 122, 146, 150, 170, 182, 210, 222, 250, 254 };
+	private byte[][] filledArray;
+	private byte[][] stateArray;
+	private int width;
+	private int length;
+	private int widthCount;
+	private int lengthCount;
 
 	public Rule1D(int rule) throws NotValidRuleException {
 		setRule(rule);
+		this.width = 40;
+		this.length = 75;
+		filledArray = new byte[length][width];
+		stateArray = new byte[length][width];
+	}
 
+	public Rule1D(int rule, int width, int length) throws NotValidRuleException {
+		setRule(rule);
+		this.width = width;
+		this.length = length;
+		filledArray = new byte[length][width];
+		stateArray = new byte[length][width];
 	}
 
 	public void setRule(int rule) throws NotValidRuleException {
@@ -43,33 +60,116 @@ public class Rule1D {
 		}
 	}
 
-	public int getRule() {
-		return rule;
-	}
-
 	public byte isOn(byte one, byte two, byte three) throws NotValidRuleException {
 
-		if (one == 1 && two == 1 && three == 1)
+		if (one == 1 && two == 1 && three == 1) {
+			stateArray[lengthCount][widthCount] = (byte)7;
 			return state7;
-		else if (one == 1 && two == 1 && three == 0)
+		} else if (one == 1 && two == 1 && three == 0) {
+			stateArray[lengthCount][widthCount] = (byte)6;
 			return state6;
-		else if (one == 1 && two == 0 && three == 1)
+		} else if (one == 1 && two == 0 && three == 1) {
+			stateArray[lengthCount][widthCount] = (byte)5;
 			return state5;
-		else if (one == 1 && two == 0 && three == 0)
+		} else if (one == 1 && two == 0 && three == 0) {
+			stateArray[lengthCount][widthCount] = (byte)4;
 			return state4;
-		else if (one == 0 && two == 1 && three == 1)
+		} else if (one == 0 && two == 1 && three == 1) {
+			stateArray[lengthCount][widthCount] = (byte)3;
 			return state3;
-		else if (one == 0 && two == 1 && three == 0)
+		} else if (one == 0 && two == 1 && three == 0) {
+			stateArray[lengthCount][widthCount] = (byte)2;
 			return state2;
-		else if (one == 0 && two == 0 && three == 1)
+		} else if (one == 0 && two == 0 && three == 1) {
+			stateArray[lengthCount][widthCount] = (byte)1;
 			return state1;
-		else if (one == 0 && two == 0 && three == 0)
+		} else if (one == 0 && two == 0 && three == 0) {
+			stateArray[lengthCount][widthCount] = (byte)0;
 			return state0;
-		else
+		} else
 			throw new NotValidRuleException("Bad input for isOn method");
 
 	}
 
+	public void fillArrays() throws NotValidRuleException {
+
+		for (int i = 0; i < this.width; i++) {
+			int middle = this.width / 2;
+
+			if (i != middle) {
+				filledArray[0][i] = 0;
+				stateArray[0][i] = 0;
+			} else {
+				filledArray[0][i] = 1;
+				stateArray[0][i] = 1;
+			}
+		}
+		for (lengthCount = 1; lengthCount < this.length; lengthCount++) {
+			for (widthCount = 0; widthCount < this.width; widthCount++) {
+				if (widthCount == 0) {
+					filledArray[lengthCount][widthCount] = isOn((byte) 0, filledArray[lengthCount - 1][widthCount],
+							filledArray[lengthCount - 1][widthCount + 1]);
+				} else if (widthCount + 1 == width) {
+					filledArray[lengthCount][widthCount] = isOn(filledArray[lengthCount - 1][widthCount - 1],
+							filledArray[lengthCount - 1][widthCount], (byte) 0);
+				} else {
+					filledArray[lengthCount][widthCount] = isOn(filledArray[lengthCount - 1][widthCount - 1],
+							filledArray[lengthCount - 1][widthCount], filledArray[lengthCount - 1][widthCount + 1]);
+				}
+			}
+		}
+	}
+
+	public void fillArrays(byte[] firstLine) throws NotValidRuleException {
+
+		if (firstLine.length == width) {
+			for (int i = 0; i < firstLine.length; i++) {
+				filledArray[0][i] = firstLine[i];
+				stateArray[0][i] = firstLine[i];
+			}
+
+			for (lengthCount = 1; lengthCount < this.length; lengthCount++) {
+				for (widthCount = 0; widthCount < this.width; widthCount++) {
+					if (widthCount == 0) {
+						filledArray[lengthCount][widthCount] = isOn((byte) 0, filledArray[lengthCount - 1][widthCount],
+								filledArray[lengthCount - 1][widthCount + 1]);
+					} else if (widthCount == width) {
+						filledArray[lengthCount][widthCount] = isOn(filledArray[lengthCount - 1][widthCount - 1],
+								filledArray[lengthCount - 1][widthCount], (byte) 0);
+					} else {
+						filledArray[lengthCount][widthCount] = isOn(filledArray[lengthCount - 1][widthCount - 1],
+								filledArray[lengthCount - 1][widthCount], filledArray[lengthCount - 1][widthCount + 1]);
+					}
+				}
+			}
+		}
+
+		else {
+			throw new NotValidRuleException("the array lengths are off");
+		}
+	}
+	
+	public byte[] getNextFillLine(int line) throws NotValidRuleException{
+		if (line >= length)
+			throw new NotValidRuleException("line out of bounds");
+		return filledArray[line];
+	}
+
+	public byte[] getNextStateLine(int line) throws NotValidRuleException{
+		if (line >= length)
+			throw new NotValidRuleException("line out of bounds");
+		return stateArray[line];
+	}
+	
+	public byte[][] getNextLine(int line) throws NotValidRuleException{
+		if (line >= length)
+			throw new NotValidRuleException("line out of bounds");
+		
+		byte[][] next = new byte[2][width];
+		next[0] = getNextFillLine(line);
+		next[1] = getNextStateLine(line);
+		return next;
+	}
 	@Override
 	public String toString() {
 		return "rule1D [state7=" + state7 + ", state6=" + state6 + ", state5=" + state5 + ", state4=" + state4
@@ -82,6 +182,14 @@ public class Rule1D {
 
 	public int[] getRules() {
 		return rules;
+	}
+
+	public int getRule() {
+		return rule;
+	}
+
+	public int getLength() {
+		return length;
 	}
 
 }
--- src/Model/ruleModle.java
+++ src/Model/ruleModle.java
@@ -20,6 +20,35 @@ public class ruleModle
 			System.out.println(rule.isOn((byte)0,(byte)1,(byte)0));
 			System.out.println(rule.isOn((byte)0,(byte)0,(byte)1));
 			System.out.println(rule.isOn((byte)0,(byte)0,(byte)0));
+			rule.fillArrays();
+
+
+			for (int x = 0; x < rule.getLength(); x++)
+			{
+				byte[] fill  = rule.getNextFillLine(x);
+				byte[] state = rule.getNextStateLine(x);
+				for (int i = 0; i < fill.length; i ++)
+				{
+					System.out.print(fill[i]);
+				}
+				System.out.println("");
+			}
+			
+			for (int x = 0; x < rule.getLength(); x++)
+			{
+				byte[][] fill  = rule.getNextLine(x);
+				
+				for (int i = 0; i < 2; i ++)
+				{
+					for (int j = 0; j < 40; j ++)
+					{
+					System.out.print(fill[i][j]);
+					}
+				}
+				System.out.println("");
+			}
+			
+
 		} catch (NotValidRuleException e)
 		{
 			// TODO Auto-generated catch block
