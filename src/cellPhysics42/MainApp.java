d161195e1e31d941fcf38c7c01bed2ac880adc6a
Bess Burnett <bburnett2@ggc.edu>
2017-02-21 20:09:48 -0500

    custom view switches to demo

--- src/cellPhysics42/MainApp.java
+++ src/cellPhysics42/MainApp.java
@@ -6,28 +6,38 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
-	
+	private Stage primaryStage;
 
 	@Override
 	public void start(Stage primaryStage) {
-		
+		//this.primaryStage = new Stage();
 		try{
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(MainApp.class.getResource("view/DemoView.fxml"));
+		loader.setLocation(MainApp.class.getResource("view/CustomizeView.fxml"));
 		System.out.println(loader.getLocation());
 		AnchorPane pane = (AnchorPane)loader.load();
 		Scene scene = new Scene(pane);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		this.primaryStage = primaryStage;
 		}
 		
 		catch(IOException ex){
-			System.out.println("error" + ex.getLocalizedMessage());
+			System.out.println("error" + ex.getMessage());
 		}
 		
+	}
+	
+//	public void changeScene(Scene newScence){
+//		primaryStage.setScene(newScence);
+//	}
+	
+	public Stage getPrimaryStage(){
+		return primaryStage;
 	}
 
 	public static void main(String[] args) {
--- src/cellPhysics42/view/CustomizeViewControl.java
+++ src/cellPhysics42/view/CustomizeViewControl.java
@@ -1,9 +1,19 @@
 package cellPhysics42.view;
 
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import cellPhysics42.MainApp;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 
 /**Class: CustomizeViewControl.java 
    * @author Bess Burnett 
@@ -23,5 +33,24 @@ public class CustomizeViewControl extends BorderPane {
 	Label ruleNameLabel;
 	@FXML
 	GridPane binaryRuleGrid;
+	@FXML
+	private Button demoButton;
+	
+	@FXML
+	public void runDemo(){
+		try{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(MainApp.class.getResource("view/DemoView.fxml"));
+		System.out.println(loader.getLocation());
+		AnchorPane pane = (AnchorPane)loader.load();
+		Scene scene = new Scene(pane);
+		Stage newStage = (Stage) demoButton.getScene().getWindow();
+		newStage.setScene(scene);
+		}
+		
+		catch(IOException ex){
+			System.out.println("error" + ex.getMessage());
+		}
+	}
 	
 }
