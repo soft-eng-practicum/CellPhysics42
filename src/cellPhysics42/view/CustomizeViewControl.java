69e1558249c1bbf4372f132f25a75aca6ce08a87
Bess Burnett <bburnett2@ggc.edu>
2017-03-13 10:38:44 -0400

    custom view options

--- src/cellPhysics42/view/CustomizeViewControl.java
+++ src/cellPhysics42/view/CustomizeViewControl.java
@@ -1,5 +1,6 @@
 package cellPhysics42.view;
 
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -8,14 +9,14 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.security.acl.Group;
-
 import cellPhysics42.MainApp;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
@@ -32,16 +33,133 @@ import javafx.fxml.FXMLLoader;
    */
 public class CustomizeViewControl extends BorderPane {
 	@FXML
-	BorderPane mainPane;
+	private BorderPane mainPane;
 	@FXML
-	Label ruleNameLabel;
+	private Label ruleNameLabel;
+	@FXML
+	private GridPane ruleGrid;
+	@FXML
+	private GridPane squaresGrid;
 	@FXML
 	private Button demoButton;
 	@FXML
 	private Button customizeBt;
+	@FXML
+	private Button stopBt;
+	@FXML
+	private HBox numberZero;
+	@FXML
+	private HBox numberOne;
+	@FXML
+	private HBox numberTwo;
+	@FXML
+	private HBox numberThree;
+	@FXML
+	private HBox numberFour;
+	@FXML
+	private HBox numberFive;
+	@FXML
+	private HBox numberSix;
+	@FXML
+	private HBox numberSeven;
+	@FXML
+	private LabeledRectangle squareZero;
+	@FXML
+	private LabeledRectangle squareOne;
+	@FXML
+	private LabeledRectangle squareTwo;
+	@FXML
+	private LabeledRectangle squareThree;
+	@FXML
+	private LabeledRectangle squareFour;
+	@FXML
+	private LabeledRectangle squareFive;
+	@FXML
+	private LabeledRectangle squareSix;
+	@FXML
+	private LabeledRectangle squareSeven;
+	private Color oneColor;
+	private Color zeroColor;
+	private Color edgeColor;
+	
 
+	/**
+	 * Method name: initialize
+	 */
+	@FXML
+	public void initialize(){
+		setRuleNum(90);
+		edgeColor = Color.BLACK;
+		squaresGrid.setVisible(true);
+	}
 	
+	/**
+	 * Method name: setRuleNum
+	 * @param ruleNum
+	 */
+	private void setRuleNum(int ruleNum) {
+		ruleNameLabel.setText("Rule " + ruleNum);
+		setSquareNums(Integer.toBinaryString(ruleNum));
+		setPaneNumbers();
+	}
+
+	/**
+	 * Method name: setSquareNums
+	 * @param binaryString
+	 */
+	private void setSquareNums(String binaryString) {
+		while(binaryString.length() < 8){
+			binaryString = "0" + binaryString;
+		}
+		squareZero.setLabel(binaryString.charAt(7));
+		squareZero.setOutline(edgeColor);
+		squareOne.setLabel(binaryString.charAt(6));
+		squareOne.setOutline(edgeColor);
+		squareTwo.setLabel(binaryString.charAt(5));
+		squareTwo.setOutline(edgeColor);
+		squareThree.setLabel(binaryString.charAt(4));
+		squareThree.setOutline(edgeColor);
+		squareFour.setLabel(binaryString.charAt(3));
+		squareFour.setOutline(edgeColor);
+		squareFive.setLabel(binaryString.charAt(2));
+		squareFive.setOutline(edgeColor);
+		squareSix.setLabel(binaryString.charAt(1));
+		squareSix.setOutline(edgeColor);
+		squareSeven.setLabel(binaryString.charAt(0));
+		squareSeven.setOutline(edgeColor);
+	}
 	
+	/**
+	 * Method name: setPaneNumbers
+	 */
+	public void setPaneNumbers(){
+		setThreeDigBinaryNubmer(0, numberZero);
+		setThreeDigBinaryNubmer(1, numberOne);
+		setThreeDigBinaryNubmer(2, numberTwo);
+		setThreeDigBinaryNubmer(3, numberThree);
+		setThreeDigBinaryNubmer(4, numberFour);
+		setThreeDigBinaryNubmer(5, numberFive);
+		setThreeDigBinaryNubmer(6, numberSix);
+		setThreeDigBinaryNubmer(7, numberSeven);
+	}
+	
+	/**
+	 * Method name: setThreeDigBinaryNubmer
+	 * @param num
+	 * @param numberHolder
+	 */
+	public void setThreeDigBinaryNubmer(int num, HBox numberHolder){
+		ObservableList<Node> rects = numberHolder.getChildren();
+		String binNum = Integer.toBinaryString(num);
+		while(binNum.length() < 3){
+			binNum = "0" + binNum;
+		}
+		for(int index = 0; index < binNum.length(); index++){
+			((LabeledRectangle)rects.get(index)).setLabel(binNum.charAt(index));
+			((LabeledRectangle)rects.get(index)).setOutline(edgeColor);
+		}
+	}
+
 	/**
 	 * Method name: runDemo
 	 */
@@ -72,6 +190,9 @@ public class CustomizeViewControl extends BorderPane {
 		}
 	}
 	
+	/**
+	 * Method name: showCustomOptions
+	 */
 	@FXML
 	public void showCustomOptions(){
 		try{
@@ -80,6 +201,7 @@ public class CustomizeViewControl extends BorderPane {
 			AnchorPane pane = (AnchorPane)loader.load();
 			pane.setBackground(new Background(new BackgroundFill(Color.CADETBLUE, null, null)));
 			mainPane.setCenter(pane);
+			
 			System.out.println(loader.getLocation());
 		}
 		catch (Exception ex) {
--- src/cellPhysics42/view/LabeledRectangle.java
+++ src/cellPhysics42/view/LabeledRectangle.java
@@ -5,28 +5,73 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+/**Class: LabeledRectangle.java 
+   * @author Bess Burnett 
+   * @version 1.0 
+   * Course : ITEC  Spring 2017
+   * Written: Mar 2, 2017 
+  	   * 
+   * This class –  
+   * 
+   * Purpose: –  
+   */
 public class LabeledRectangle extends StackPane {
 	private Rectangle rectangle;
 	private Label number;
 	
+	/**LabeledRectangle
+	 * 
+	 * no agr constructor
+	 *
+	 * creates a LabeledRectangle object with 
+	 */
 	public LabeledRectangle(){
 		this(30.0,30.0, "1");
 	}
 	
+	/**LabeledRectangle
+	 * @param width
+	 * @param height
+	 * @param lString
+	 * 3 agr constructor
+	 *
+	 * creates a LabeledRectangle object with 
+	 */
 	public LabeledRectangle(double width, double height, String lString){
 		rectangle = new Rectangle(width, height);
 		number = new Label(lString);
 		super.getChildren().addAll(rectangle, number);
-		super.setWidth(width);
-		super.setHeight(height);
+		super.setWidth(width + 2.0);
+		super.setHeight(height + 2.0);
 		setFill(Color.CYAN);
 	}
 	
+	/**
+	 * Method name: setFill
+	 * @param color
+	 */
 	public void setFill(Color color){
 		rectangle.setFill(color);
 	}
 	
+	/**
+	 * Method name: setLabel
+	 * @param text
+	 */
 	public void setLabel(String text){
 		number.setText(text);
+	}
+	
+	public void setOutline(Color color){
+		rectangle.setStroke(color);
+		rectangle.setStrokeWidth(2.0);
+	}
+	
+	/**
+	 * Method name: setLabel
+	 * @param text
+	 */
+	public void setLabel(char text){
+		number.setText(text + "");
 	}
 }
